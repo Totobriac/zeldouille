346088f89b0951e79642c73a07dbd67e16cca2c8
Your Name <you@example.com>
2021-11-10 20:16:06 +0100

    moving map

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import {drawTiles} from "./overWorld.js";
+import { drawTiles, moveDown, moveUp } from "./overWorld.js";
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
@@ -6,8 +6,20 @@ var ctx = canvas.getContext("2d");
 canvas.width = 1200;
 canvas.height = 400;
 
+window.addEventListener('keydown', function (e) {
+  if (e.code === "ArrowDown") {
+    moveDown();
+  }
+  if (e.code === "ArrowUp") {
+    moveUp();
+  }
+});
+
 function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawTiles(ctx);
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, canvas.width, 8);
   requestAnimationFrame(animate);
 }
 
