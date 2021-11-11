7e63adf9b9bc57d67de6938fd73f9b877a2c21f1
Your Name <you@example.com>
2021-11-11 18:11:32 +0100

    nav ok

--- overWorld.js
+++ overWorld.js
@@ -1,42 +1,19 @@
+import { mainMap } from "./maps.js";
+
 var tiles = new Image();
 tiles.src = "./overworldtiles_no_space.png";
 
-var offset = 0;
+var xOffset = 0;
+var yOffset = 0;
 var mapTiles = 336;
-var map1 =
-  [19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19,
-    19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 7, 2, 7, 2,
-    2, 2, 2, 46, 2, 2, 46, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 7, 2, 2,
-    2, 2, 46, 46, 2, 46, 2, 2, 2, 2, 2, 3, 4, 5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 46, 2, 2, 46, 2, 2, 2, 2, 2, 9, 10, 11, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 2, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13,
-    19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 2, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19,
-  ];
-
-var map2 =
-  [19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 2, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19,
-    19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 2, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 7, 2, 7, 2,
-    2, 2, 2, 46, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 7, 2, 2,
-    2, 2, 46, 46, 2, 2, 2, 2, 2, 2, 2, 3, 4, 5, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 46, 2, 2, 2, 2, 2, 2, 2, 2, 9, 10, 11, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
-    13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13,
-    19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19,
-  ];
-
-var mainMap = [map1, map2];
-var oldMap = map1;
+var oldMap = mainMap[1];
 var newMap;
 var direction = 0;
+var upDown = 392;
+var leftRight = 904;
+
+var zob = 0;
+
 
 function getTile(tile) {
   var line = Math.floor(tile / 6);
@@ -46,7 +23,7 @@ function getTile(tile) {
 
 function drawTiles(ctx) {
 
-  offset += direction;
+  upDown === 8 ? xOffset += direction : yOffset += direction;
 
   ctx.fillStyle = "rgb(116,116,116)";
   ctx.fillRect(8, 8, 896, 384);
@@ -55,31 +32,63 @@ function drawTiles(ctx) {
     var selectedTile = getTile(oldMap[i]);
     var line = Math.floor(i / 28);
     var column = i - (line * 28);
-    if (Math.floor(line * 32 + 8 + offset) > -24) {
-      ctx.drawImage(tiles, selectedTile[1], selectedTile[0], 16, 16, Math.floor(column * 32 + 8), Math.floor(line * 32 + 8 + offset), 32, 32);
-    }
+    ctx.drawImage(tiles, selectedTile[1], selectedTile[0], 16, 16,
+      Math.floor(column * 32 + 8 + xOffset), Math.floor(line * 32 + 8 + yOffset), 32, 32);
     if (newMap) {
       var selectedTile = getTile(newMap[i]);
       var line = Math.floor(i / 28);
       var column = i - (line * 28);
-      ctx.drawImage(tiles, selectedTile[1], selectedTile[0], 16, 16, Math.floor(column * 32 + 8), Math.floor(line * 32 + 400 + offset), 32, 32);
+      ctx.drawImage(tiles, selectedTile[1], selectedTile[0], 16, 16,
+        Math.floor(column * 32 + 8 - zob + leftRight + xOffset), Math.floor(line * 32 + upDown + yOffset), 32, 32);
     }
-    if (offset === 400 || offset === -400) {
-      offset = 0;
+    if (yOffset === 390 || yOffset === -390) {
+      yOffset = 0;
+      oldMap = newMap;
+      direction = 0;
+    }
+    if (xOffset === 888 || xOffset === -896) {
+      yOffset = 0;
       oldMap = newMap;
       direction = 0;
     }
   }
+  console.log(xOffset);
 }
 
 function moveDown() {
-  newMap = map2;
+  newMap = mainMap[1];
   direction = -2;
+  upDown = 392;
+  leftRight = 0;
+  xOffset = 0;  
+  zob = 0;
 }
 
 function moveUp() {
-  newMap = map1;
+  newMap = mainMap[0];
   direction = 2;
+  upDown = -376;
+  leftRight = 0;
+  xOffset = 0;
+  zob = 0;
 }
 
-export { drawTiles, moveDown, moveUp };
+function moveRight() {
+  newMap = mainMap[0];
+  direction = -2;
+  upDown = 8;
+  leftRight = 904;
+  yOffset = 0;
+  zob = 8;
+}
+
+function moveLeft() {
+  newMap = mainMap[1];
+  direction = 2;
+  upDown = 8;
+  leftRight = -888;
+  yOffset = 0;
+  zob = 0;
+}
+
+export { drawTiles, moveDown, moveUp, moveRight, moveLeft };
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { drawTiles, moveDown, moveUp } from "./overWorld.js";
+import { drawTiles, moveDown, moveUp, moveRight, moveLeft } from "./overWorld.js";
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
@@ -13,6 +13,12 @@ window.addEventListener('keydown', function (e) {
   if (e.code === "ArrowUp") {
     moveUp();
   }
+  if (e.code === "ArrowRight") {
+    moveRight();
+  }
+  if (e.code === "ArrowLeft") {
+    moveLeft();
+  }
 });
 
 function animate() {
@@ -20,6 +26,9 @@ function animate() {
   drawTiles(ctx);
   ctx.fillStyle = "white";
   ctx.fillRect(0, 0, canvas.width, 8);
+  ctx.fillRect(0, 392, canvas.width, 8);
+  ctx.fillRect(0, 0, 8, canvas.height);
+  ctx.fillRect(898, 0, 302,canvas.height)
   requestAnimationFrame(animate);
 }
 
