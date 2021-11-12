8fef50c116d625e9e9aee3a711bea39d470b1776
Toto Briac <totobriac@gmail.com>
2021-11-12 21:48:08 +0100

    he is moving!

--- hero.js
+++ hero.js
@@ -3,24 +3,32 @@ class Hero {
     this.x = x;
     this.y = y;
     this.spriteSize = spriteSize;
-    this.nextX = 0;
-    this.nextY = 0;
+    this.nextX = x;
+    this.nextY = y;
   }
   moveUp(map) {
     this.nextY = this.y - this.spriteSize;
-    var nexTile = this.nextTile(map);
-    if (map[nexTile] == 2) this.y -= this.spriteSize;
+    this.nextX = this.x;
+    var nextTile = this.nextTile(map);
+    if (map[nextTile] == 2) this.y -= this.spriteSize;
   }
   moveDown(map) {
     this.nextY = this.y + this.spriteSize;
-    var nexTile = this.nextTile(map);
-    if (map[nexTile] == 2) this.y += this.spriteSize;
+    this.nextX = this.x;
+    var nextTile = this.nextTile(map);
+    if (map[nextTile] == 2) this.y += this.spriteSize;
   }
-  moveRight() {
-    this.x += this.spriteSize;
+  moveRight(map) {
+    this.nextX = this.x + this.spriteSize;
+    this.nextY = this.y;
+    var nextTile = this.nextTile(map);
+    if (map[nextTile] == 2) this.x += this.spriteSize;
   }
-  moveLeft() {
-    this.x -= this.spriteSize;
+  moveLeft(map) {
+    this.nextX = this.x - this.spriteSize;
+    this.nextY = this.y;
+    var nextTile = this.nextTile(map);
+    if (map[nextTile] == 2) this.x -= this.spriteSize;
   }
   nextTile(map) {
     return (Math.floor(((this.nextY - 8) / this.spriteSize) * 28) + Math.floor(this.nextX / this.spriteSize));
--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var ctx = canvas.getContext("2d");
 canvas.width = 1200;
 canvas.height = 400;
 
-var zelda = new Hero(8,72,32);
+var zelda = new Hero(8, 72, 32);
 
 window.addEventListener('keydown', function (e) {
   if (e.code === "ArrowDown") {
@@ -17,10 +17,10 @@ window.addEventListener('keydown', function (e) {
     zelda.moveUp(oldMap);
   }
   if (e.code === "ArrowRight") {
-    zelda.moveRight();
+    zelda.moveRight(oldMap);
   }
   if (e.code === "ArrowLeft") {
-    zelda.moveLeft();
+    zelda.moveLeft(oldMap);
   }
 });
 
@@ -32,10 +32,10 @@ function animate() {
   ctx.fillRect(0, 0, canvas.width, 8);
   ctx.fillRect(0, 392, canvas.width, 8);
   ctx.fillRect(0, 0, 8, canvas.height);
-  ctx.fillRect(904, 0, 302,canvas.height);
-  
+  ctx.fillRect(904, 0, 302, canvas.height);
+
   ctx.fillStyle = "green";
-  ctx.fillRect(zelda.x , zelda.y, zelda.spriteSize, zelda.spriteSize)
+  ctx.fillRect(zelda.x, zelda.y, zelda.spriteSize, zelda.spriteSize)
   zelda.nextTile(oldMap);
 
   requestAnimationFrame(animate);
