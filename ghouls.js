7c673030054b980779f12d4dba8fed44eb34102b
Toto Briac <totobriac@gmail.com>
2021-11-15 06:42:18 +0100

    delbor but fine

--- ghouls.js
+++ ghouls.js
@@ -10,11 +10,10 @@ class Monster {
   }
   randomDirection() {
     this.direction = Math.floor(Math.random() * 4);
-    console.log(this.direction)
   }
   move() {
     this.index = Math.floor(((this.y - 8) / 32) * 28) + Math.floor((this.x - 8) / 32);
-    console.log(this.direction)
+
     if (this.direction === 0) {
       var isExiting = checkExit(this.index + 1);
       if (this.map[this.index + 1] === 2 && isExiting === undefined) {
@@ -31,15 +30,20 @@ class Monster {
     }
     if (this.direction === 2) {
       var isExiting = checkExit(this.index + 1);
-      if (this.map[this.index + 1] === 2 && isExiting === undefined) {
-        this.y += 1;
+      var nextY = this.y + 32;
+      var next = nextTile(this.map, this.x, nextY);
+      console.log(next)
+      if (this.map[next] === 2 && isExiting === undefined) {
+        while (this.y < nextY) this.y += 0.00001;
       }
       else { this.randomDirection() }
     }
     if (this.direction === 3) {
       var isExiting = checkExit(this.index);
-      if (this.map[this.index] === 2 && isExiting === undefined) {
-        this.y -= 1;
+      var nextY = this.y - 32;
+      var next = nextTile(this.map, this.x, nextY);
+      if (this.map[next] === 2 && isExiting === undefined) {
+        while (this.y > nextY) this.y -= 0.00001;
       }
       else { this.randomDirection() }
     }
@@ -49,15 +53,15 @@ class Monster {
 
 function spawnMonsters(map) {
   var monsters = [];
-  for (let i = 0; monsters.length < 1; i++) {
+  for (let i = 0; monsters.length < 6; i++) {
     var monster = new Monster(map);
     if (map[monster.index] === 2) monsters.push(monster)
   }
   return monsters;
 }
 
-function nextTile(map) {
-  return (Math.floor(((this.nextY - 8) / this.spriteSize) * 28) + Math.floor(this.nextX / this.spriteSize));
+function nextTile(map, x, y) {
+  return (Math.floor(((y - 8) /32) * 28) + Math.floor(x /32));
 }
 
 
