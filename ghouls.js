9100413eeceb157e7719f707ade693819585f0a4
Your Name <you@example.com>
2021-11-15 21:57:42 +0100

    to fast up/down

--- ghouls.js
+++ ghouls.js
@@ -4,46 +4,53 @@ class Monster {
   constructor(map) {
     this.x = (Math.floor(Math.random() * 24) + 2) * 32 + 8;
     this.y = (Math.floor(Math.random() * 10) + 1) * 32 + 8;
-    this.index = Math.floor(((this.y - 8) / 32) * 28) + Math.floor((this.x - 8) / 32);
     this.map = map;
     this.direction = Math.floor(Math.random() * 4);
+    this.index = this.getIndex();
   }
   randomDirection() {
     this.direction = Math.floor(Math.random() * 4);
   }
+  getIndex() {
+    return (Math.floor(((this.y - 8) / 32) * 28) + Math.floor((this.x - 8) / 32));
+  }
   move() {
-    this.index = Math.floor(((this.y - 8) / 32) * 28) + Math.floor((this.x - 8) / 32);
+    this.index = this.getIndex();
+    var isExiting;
+    this.direction === 0 || this.direction === 2
+      ? isExiting = checkExit(this.index + 1)
+      : isExiting = checkExit(this.index);
 
     if (this.direction === 0) {
-      var isExiting = checkExit(this.index + 1);
       if (this.map[this.index + 1] === 2 && isExiting === undefined) {
         this.x += 1;
       }
       else { this.randomDirection() }
     }
     if (this.direction === 1) {
-      var isExiting = checkExit(this.index);
       if (this.map[this.index] === 2 && isExiting === undefined) {
         this.x -= 1;
       }
       else { this.randomDirection() }
     }
     if (this.direction === 2) {
-      var isExiting = checkExit(this.index + 1);
-      var nextY = this.y + 32;
-      var next = nextTile(this.map, this.x, nextY);
-      console.log(next)
-      if (this.map[next] === 2 && isExiting === undefined) {
-        while (this.y < nextY) this.y += 0.00001;
-      }
-      else { this.randomDirection() }
+      if (this.map[this.index + 28] === 2 && isExiting === undefined) {
+
+        this.y += 32;
+        console.log(this.index);
+        }
+        // var nextY = this.y + 32;
+        // var next = nextTile(this.map, this.x, nextY);
+        // if (this.map[next] === 2 && isExiting === undefined) {
+        //   this.y += 32;
+        // }
+        else { this.randomDirection() }
     }
     if (this.direction === 3) {
-      var isExiting = checkExit(this.index);
       var nextY = this.y - 32;
       var next = nextTile(this.map, this.x, nextY);
       if (this.map[next] === 2 && isExiting === undefined) {
-        while (this.y > nextY) this.y -= 0.00001;
+        this.y -= 32;
       }
       else { this.randomDirection() }
     }
@@ -53,7 +60,7 @@ class Monster {
 
 function spawnMonsters(map) {
   var monsters = [];
-  for (let i = 0; monsters.length < 6; i++) {
+  for (let i = 0; monsters.length < 8; i++) {
     var monster = new Monster(map);
     if (map[monster.index] === 2) monsters.push(monster)
   }
@@ -61,7 +68,7 @@ function spawnMonsters(map) {
 }
 
 function nextTile(map, x, y) {
-  return (Math.floor(((y - 8) /32) * 28) + Math.floor(x /32));
+  return (Math.floor(((y - 8) / 32) * 28) + Math.floor(x / 32));
 }
 
 
