838f8cc92bb0c7170d8a8b8e4faaa5246e3ce6b6
Your Name <you@example.com>
2021-11-18 09:34:33 +0100

    monsters ok now zelda moves

--- ghouls.js
+++ ghouls.js
@@ -1,15 +1,15 @@
-import { checkExit } from "./maps.js";
 
 class Monster {
-  constructor(map, obstacles, ctx) {
+  constructor(map, obstacles, bundaries) {
     this.x = (Math.floor(Math.random() * 24) + 2) * 32 + 8;
     this.y = (Math.floor(Math.random() * 10) + 1) * 32 + 8;
     this.map = map;
     this.obstacles = obstacles;
     this.direction = Math.floor(Math.random() * 4);
     this.index = this.getIndex();
-    this.ctx = ctx;
     this.isColliding = false;
+    this.isExiting = false;
+    this.bundaries = bundaries;
   }
   randomDirection() {
     this.direction = Math.floor(Math.random() * 4);
@@ -18,25 +18,30 @@ class Monster {
     return (Math.floor((this.y - 8) / 32) * 28 + Math.floor((this.x - 8) / 32));
   }
   move() {
-    var index = this.getIndex();
-    var exit = checkExit(index);
-    if (exit) this.randomDirection();
     if (!this.isColliding) {
       if (this.direction === 0) {
-        this.isColliding = this.checkCollision(this.x + 1, this.y);
-        if (this.isColliding === false) this.x += 1;
+        var nextX = this.x + 1;
+        var exit = this.checkBundaries(nextX, this.y);
+        this.isColliding = this.checkCollision(nextX, this.y);
+        this.isColliding === false && exit === false ? this.x += 1 : this.randomDirection();
       }
       else if (this.direction === 1) {
-        this.isColliding = this.checkCollision(this.x - 1, this.y);
-        if (this.isColliding === false) this.x -= 1;
+        var nextX = this.x - 1;
+        var exit = this.checkBundaries(nextX, this.y);
+        this.isColliding = this.checkCollision(nextX, this.y);
+        this.isColliding === false && exit === false ? this.x -= 1 : this.randomDirection();
       }
       else if (this.direction === 2) {
-        this.isColliding = this.checkCollision(this.x, this.y + 1);
-        if (this.isColliding === false) this.y += 1;
+        var nextY = this.y + 1;
+        var exit = this.checkBundaries(this.x, nextY);
+        this.isColliding = this.checkCollision(this.x, nextY);
+        this.isColliding === false && exit === false ? this.y += 1 : this.randomDirection();
       }
       else if (this.direction === 3) {
-        this.isColliding = this.checkCollision(this.x, this.y - 1);
-        if (this.isColliding === false) this.y -= 1;
+        var nextY = this.y - 1;
+        var exit = this.checkBundaries(this.x, nextY);
+        this.isColliding = this.checkCollision(this.x, nextY);
+        this.isColliding === false && exit === false ? this.y -= 1 : this.randomDirection();
       }
     }
     else {
@@ -58,6 +63,18 @@ class Monster {
     }
     return colliding
   }
+  checkBundaries(x, y) {
+    if (x >= this.bundaries[0] * 32 + 8 &&
+      x <= (27 - this.bundaries[1]) * 32 + 8 &&
+      y >= this.bundaries[2] * 32 + 8 &&
+      y <= (11 - this.bundaries[3]) * 32 + 8
+    ) {
+      return false
+    }
+    else {
+      return true
+    }
+  }
 }
 
 function spawnMonsters(map, ctx) {
@@ -71,7 +88,7 @@ function spawnMonsters(map, ctx) {
   }
   var monsters = [];
   for (let i = 0; monsters.length < 8; i++) {
-    var monster = new Monster(map, obstacles, ctx);
+    var monster = new Monster(map, obstacles, [1, 1, 1, 1]);
     if (map[monster.index] === 2) monsters.push(monster)
   }
   return monsters;
